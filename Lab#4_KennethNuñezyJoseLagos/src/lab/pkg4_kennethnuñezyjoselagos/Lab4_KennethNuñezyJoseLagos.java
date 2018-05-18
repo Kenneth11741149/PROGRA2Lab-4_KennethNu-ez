781478f16482f9c17de1577ef503e4271b9fcda0
Kenneth Nuñez <Kenneth11741149>
2018-05-18 13:53:30 -0600

    Creacion del main

--- Lab#4_KennethNuñezyJoseLagos/src/lab/pkg4_kennethnuñezyjoselagos/Lab4_KennethNuñezyJoseLagos.java
+++ Lab#4_KennethNuñezyJoseLagos/src/lab/pkg4_kennethnuñezyjoselagos/Lab4_KennethNuñezyJoseLagos.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
  * @author Burni
  */
 public class Lab4_KennethNuñezyJoseLagos {
-
     
     public static void main(String[] args) {
-        System.out.println("Hola Tali");
+        System.out.println("Bienvenido al laboratorio de Jose Lagos 11741409");
+        System.out.println("Bienvenido al laboratorio de Kenneth Nuñez 11741149");
+        char resp = 'S';
+        while(resp == 'S'||resp == 's'){
+            System.out.println("***Menu Inicial***");
+            System.out.println("1. Jugadores");
+            System.out.println("2. Jugar");
+        }
     }
     
 }
