9433d8ec0610fe4f9ce5f3aea49c7d8e7fab7852
Tali98 <tali_lagos@unitec.edu>
2018-05-18 17:34:32 -0600

    move mago

--- Lab#4_KennethNuñezyJoseLagos/src/lab/pkg4_kennethnuñezyjoselagos/dragon.java
+++ Lab#4_KennethNuñezyJoseLagos/src/lab/pkg4_kennethnuñezyjoselagos/dragon.java
@@ -5,6 +5,8 @@
  */
 package lab.pkg4_kennethnuñezyjoselagos;
 
+import static lab.pkg4_kennethnuñezyjoselagos.mago.read;
+
 /**
  *
  * @author tali_
@@ -25,7 +27,30 @@ public class dragon extends Piezas {
     
     @Override
     public void movement(Piezas[][]Tablero, int x,int y,int a,int b) {
-    
+      boolean v = false;
+      while (v = false) {
+                    if (((Piezas) this).color.equals((Tablero[x][y]).color)) {
+                        System.out.println("No puede comerse a su propia pieza");
+                        System.out.println("ingrese x");
+                        x = read.nextInt();
+                        System.out.println("ingrese y");
+                        y = read.nextInt();
+                        v = false;
+                    } else {
+                        if ((x <= 9 && y <= 9) && (x >= 0 && y > +0)) {
+                            Tablero[x][y] = this;
+                            Tablero[a][b] = null;
+                            v = true;
+                        } else {
+                            System.out.println("Numeros illegales, ingrese nuevos");
+                            System.out.println("Ingrese x");
+                            x = read.nextInt();
+                            System.out.println("Ingrese y");
+                            y = read.nextInt();
+                            v = false;
+                        }
+                    }
+                }
     }
     
     
